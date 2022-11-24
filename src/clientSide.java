f881ae3e6e38a3bb39fe79eb44f40b48c052b650
GabrielDorsey <gcd0003@uah.edu>
2022-11-24 12:25:16 -0600

    Commented out code and added a send File button to be used in further implementation.

--- src/clientSide.java
+++ src/clientSide.java
@@ -4,11 +4,12 @@ import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
 
-public class clinetUI extends JFrame{
+public class clientSide extends JFrame{
     private JPanel mainPanel;
     private JTextField textBox;
     private JButton sendButton;
     private  JTextArea chatArea;
+    private JButton sendFileButton;
     private  static Socket socket;
     private static InputStreamReader inStream;
     private  static BufferedWriter buffWriter;
@@ -16,12 +17,18 @@ public class clinetUI extends JFrame{
 
 
 
-
-    public clinetUI(String title){
-        super(title);
+    /**
+     * @param title gets the title so that the frame can update it when the program is ran.
+     *              Function: Creates the server side UI to be used throughout the program
+     *
+     */
+    public clientSide(String title){
+        super(title); //gets the title of the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+
+        //when the send button is pressed, retreive the message that was in the text box and send the message.
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -38,6 +45,13 @@ public class clinetUI extends JFrame{
         });
     }
 
+
+
+    /**
+     * Method: sendMessage
+     * Function: Sends a message to the client
+     * @param message : Message to send to the client
+     */
     public void sendMessage(String message){
         try{
             buffWriter.write(message);
@@ -50,6 +64,13 @@ public class clinetUI extends JFrame{
         }
     }
 
+    /**
+     * Method: closeConnection
+     * Function: Closes the connection of all the thing that the server uses, and throws and exception if it does not work
+     * @param socket  Used to close the socket connection
+     * @param buffReader Used to close the reader Connection
+     * @param buffWriter Used to close the writer
+     */
     public static void closeConnection(Socket socket, BufferedReader buffReader, BufferedWriter buffWriter){
         try{
             if(socket!= null){
@@ -67,14 +88,19 @@ public class clinetUI extends JFrame{
         }
     }
 
+
+    /**
+     * Main Method that handles the runtime of the program
+     * Function: Will load the JFrame so that the user can type out to the screen and send messages.
+     * Will attempt to connect to the server so that the client and send messages to the server and can receive
+     * messages to the server.
+     */
     public static void main(String[] args){
-        clinetUI mainFrame =  new clinetUI("Client Side App");
+        clientSide mainFrame =  new clientSide("Client Side App");
         mainFrame.setVisible(true);
-
-
-
-
         try {
+
+            //Attempts to establish a connection to the server, throws an exception if not possible.
             try {
                 socket = new Socket("localhost",12345);
                 buffReader = new BufferedReader( new InputStreamReader(socket.getInputStream()));
@@ -85,8 +111,10 @@ public class clinetUI extends JFrame{
                 e.printStackTrace();
                 closeConnection(socket, buffReader,buffWriter);
             }
+
+            //While the message from the server is not "null", update the receive the message from the client and update the chat box.
             String fromServer = "";
-            while(!fromServer.equals("exit")) {
+            while(!fromServer.equals("null")) {
                 fromServer = buffReader.readLine();
                 mainFrame.chatArea.setText(mainFrame.chatArea.getText().trim()+ "\n Server:\t" + fromServer );
             }
